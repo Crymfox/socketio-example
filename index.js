40c0d5791c51d477df2174cdbb0fb36620020c8a
crymfox <sameur.bh@gmail.com>
2023-02-03 13:30:06 -0800

    screw it

---
+++ index.js
@@ -0,0 +1,36 @@
+// import express from 'express';
+const express = require('express');
+// import http from 'http';
+const http = require('http');
+// import { Server } from "socket.io";
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+//   res.send('Hello World!');
+// });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('user connection', 'a user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    io.emit('user connection', 'a user connected');
+});
+
+server.listen(5000, () => {
+  console.log('listening on *:5000');
+});
